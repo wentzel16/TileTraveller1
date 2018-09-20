46a59c6be7006d8998a29af3bd0dbe6fa29e8613
wentzel16 <43389665+wentzel16@users.noreply.github.com>
2018-09-20 17:41:53 +0000

    Add files via upload
    Var að vista skjalið mitt í git

---
+++ TileTraveller.py
@@ -0,0 +1,10 @@
+#Tile Traveller board game first half
+
+Direction=input(str("Insert a direction N, S, E or W: ")).lower()
+
+
+if Direction == "n":
+    print("you can travel (n)orth or (e)ast or (s)outh")
+else: print("not a valid direction!")
+
+
